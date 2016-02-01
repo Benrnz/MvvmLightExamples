8623a51289abce53edc0d2dc410f6c5627bfe2ec
benrnz <ben@rees.biz>
2016-02-01 17:48:05 +1300

    Initial Commit

---
+++ MvvmLightWithXBind/MvvmLightWithXBind/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Windows.UI.Core;
+using Windows.UI.Xaml.Navigation;
+using MvvmLight3.ViewModel;
+
+namespace MvvmLight3
+{
+    public sealed partial class MainPage
+    {
+        public MainViewModel Vm => (MainViewModel)DataContext;
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Mvvm Light: Ben: Not sure what this does, maybe allow back to exit app?
+            SystemNavigationManager.GetForCurrentView().BackRequested += SystemNavigationManagerBackRequested;
+
+            Loaded += (s, e) =>
+            {
+                Vm.RunClock();
+            };
+        }
+
+        private void SystemNavigationManagerBackRequested(object sender, BackRequestedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                e.Handled = true;
+                Frame.GoBack();
+            }
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            Vm.StopClock();
+            base.OnNavigatingFrom(e);
+        }
+    }
+}
